cc6179ac077216bcae2c87cb4220603a5cbdd1e1
developermartins <martssilvapro@gmail.com>
2024-03-06 15:40:57 -0300

    unused code removed

--- src/api/adm.ts
+++ src/api/adm.ts
@@ -13,33 +13,3 @@ export const registerUser = async (data: any) => {
 
     return res;
 };
-
-export const forgotPassword = async (data: string) => {
-    const res = await api.post('/forgot_password', data);
-
-    return res;
-};
-
-export const resetPassword = async (data: object) => {
-    const res = await api.put('/reset_password', data);
-
-    return res;
-};
-
-export const updateEmail = async (id: string, data: object, token: string) => {
-    const res = await api.put(`/user/update/email/${ id }`, data, { headers: { Authorization: `Bearer ${token}` } });
-
-    return res;
-};
-
-export const updateUsername = async (id: string, data: object, token: string) => {
-    const res = await api.put(`/user/update/username/${ id }`, data, { headers: { Authorization: `Bearer ${token}` } });
-
-    return res;
-};
-
-export const updatePassword = async (id: string, data: object, token: string) => {
-    const res = await api.put(`/user/update/password/${ id }`, data, { headers: { Authorization: `Bearer ${token}` } });
-
-    return res;
-};
